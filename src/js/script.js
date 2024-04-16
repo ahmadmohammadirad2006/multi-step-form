5e746e04062d905d3ffebb3d34b36f3562ce66fd
ahmadmohammadirad2006 <ahmadmohammadirad2006@gmail.com>
2024-04-16 22:15:16 +0330

    feat: add app logic (js part) for step 2
    Changes:
     - Add planTerm variable to know the state of plan term
     - Add functionality to change plan term
     - Add functionality to select plan
     - Add functionality to go 3rd step by click on next step button in step 2
     - Make GoToNextStep method dynamic and change its name to GoToStep

--- src/js/script.js
+++ src/js/script.js
@@ -3,14 +3,17 @@
 const submitFormBtnEl = document.getElementById("submitFormBtn");
 const formEl = document.getElementById("form");
 const stpesListEl = document.getElementById("stepsList");
-
+const changePlanTermBtnEl = document.getElementById("changePlanTermBtn");
+const planListEl = document.getElementById("planList");
+const planNextStepBtnEl = document.getElementById("planNextStepBtn");
 // FUNCTIONS
 
 class App {
   step = 1;
-
+  planTerm = "yearly";
   constructor() {
     // ATTACH EVENT HANDLERS
+    // Submit Form
     submitFormBtnEl.addEventListener(
       "click",
       function () {
@@ -43,10 +46,79 @@ class App {
 
         if (isFromValid) {
           // Go to next step
-          this.GoToNextStep();
+          this.GoToStep(2);
         }
       }.bind(this)
     );
+
+    // Change Plan Time
+    changePlanTermBtnEl.addEventListener(
+      "click",
+      function () {
+        const cricle = document.getElementById("circle");
+        const [monthlyTextEl, yearlyTextEl] = changePlanTermBtnEl
+          .closest("div")
+          .querySelectorAll("small");
+        const planListItemInfoEls = [...planListEl.querySelectorAll("li div")];
+
+        console.log(monthlyTextEl, yearlyTextEl);
+
+        // Switch to other term option
+        this.planTerm = this.planTerm === "yearly" ? "monthly" : "yearly";
+
+        if (this.planTerm === "yearly") {
+          cricle.classList.remove("translate-x-[-.53rem]");
+          cricle.classList.add("translate-x-[.53rem]");
+
+          monthlyTextEl.classList.remove("text-marineBlue");
+          yearlyTextEl.classList.add("text-marineBlue");
+
+          planListItemInfoEls.forEach((item) => {
+            item.querySelector("span:nth-child(3)").style.display = "inline";
+            item
+              .querySelector("span:nth-child(2)")
+              .querySelector("span").textContent =
+              +item.querySelector("span:nth-child(2)").querySelector("span")
+                .textContent * 10;
+          });
+        }
+        if (this.planTerm === "monthly") {
+          cricle.classList.remove("translate-x-[.53rem]");
+          cricle.classList.add("translate-x-[-.53rem]");
+
+          monthlyTextEl.classList.add("text-marineBlue");
+          yearlyTextEl.classList.remove("text-marineBlue");
+
+          planListItemInfoEls.forEach((item) => {
+            item.querySelector("span:nth-child(3)").style.display = "none";
+            item
+              .querySelector("span:nth-child(2)")
+              .querySelector("span").textContent =
+              +item.querySelector("span:nth-child(2)").querySelector("span")
+                .textContent / 10;
+          });
+        }
+      }.bind(this)
+    );
+
+    // Selecet Plan
+    planListEl.addEventListener("click", function (e) {
+      const planEl = e.target.closest("li");
+      const planListItemEls = planListEl.querySelectorAll("li");
+      if (!planEl) return;
+      planListItemEls.forEach((plan) =>
+        plan.classList.remove("plan-item-active")
+      );
+      planEl.classList.add("plan-item-active");
+    });
+
+    // Go to 3rd step
+    planNextStepBtnEl.addEventListener("click" , function(){
+      this.GoToStep(3)
+    }.bind(this))
+
+    
+
   }
 
   rederErrorInput(input, msg) {
@@ -75,30 +147,32 @@ class App {
     if (!prefix || !domain) return false;
     return true;
   }
+
   // It needs to be improved later, for now it just check if the input is a positive number
   checkPhone(num) {
     if (!Number.isFinite(+num) || !(+num > -1)) return false;
     return true;
   }
 
-  GoToNextStep() {
+  GoToStep(step) {
     const prevStepNumEl = stpesListEl.querySelector(
       `[data-step="${this.step}"]`
     );
     prevStepNumEl.classList.remove("step-item-active");
 
-    const prevStepSecEl = document.querySelector(`section[data-step="${this.step}"]`);
+    const prevStepSecEl = document.querySelector(
+      `section[data-step="${this.step}"]`
+    );
     prevStepSecEl.classList.add("hidden");
-    console.log(prevStepSecEl)
+    console.log(prevStepSecEl);
 
+    this.step = step;
 
-    this.step++;
-
-
-    const nextStepSecEl = document.querySelector(`section[data-step="${this.step}"]`);
+    const nextStepSecEl = document.querySelector(
+      `section[data-step="${this.step}"]`
+    );
     nextStepSecEl.classList.remove("hidden");
-    console.log(nextStepSecEl)
-
+    console.log(nextStepSecEl);
 
     const nextStepNumEl = stpesListEl.querySelector(
       `[data-step="${this.step}"]`
